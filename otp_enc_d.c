4bf28476a9886617c63665d8e3294ce9dd353be3
Andrew Brown <andrewbrown600@gmail.com>
2016-05-29 14:53:03 -0700

    added send file function

--- otp_enc_d.c
+++ otp_enc_d.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <ctype.h>
 
 void error(const char *msg)
 {
@@ -14,79 +15,107 @@ void error(const char *msg)
     exit(1);
 }
 
-int main(int argc, char *argv[])
-{
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
-     char buffer[1024];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n, rv;
-     int yes = 1;
-     fd_set readfds;
-     struct timeval tv;
-
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-
-     //allows us to reuse the socket
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-        perror("setsockopt");
-        exit(1);
-    }
-
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0)
-          error("ERROR on accept");
+char* getFile(int connection){
+    char buffer[1024];
+    int rv;
+    fd_set readfds;
+    struct timeval tv;
 
     //clear the file descriptors
     FD_ZERO(&readfds);
     //add our sockfd to the fd set
-    FD_SET(newsockfd, &readfds);
+    FD_SET(connection, &readfds);
     tv.tv_sec = 0;
     tv.tv_usec = 100000;
 
     //file to temporarily store stuff
     FILE *fp;
-
-    fp = fopen("temp", "w+");
+    //will use for temporary file
+    int size = 20;
+    char *tempname = malloc(size);
+    char *name = "temp_plaintext";
+    int pid = getpid();
+    snprintf(tempname, size, "%s%d", name, pid);
+    fp = fopen(tempname, "w+");
     //loop to accept incoming messages
-     while (1) {
-         bzero(buffer,1024);
-         rv = select(newsockfd+1, &readfds, NULL, NULL, &tv);
-         if (rv == -1) {
-             perror("select"); // error occurred in select()
-         } else if (rv == 0) {
-             break;
-         } else {
-             // if there is data to be read
-             if (FD_ISSET(newsockfd, &readfds)) {
-                 recv(newsockfd, buffer, 1024, 0);
-                 fputs(buffer, fp);
-             }
-         }
-     }
-     fclose(fp);
-     bzero(buffer,1024);
-     //:q!printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return 0;
+    while (1) {
+       bzero(buffer,1024);
+       rv = select(connection+1, &readfds, NULL, NULL, &tv);
+       if (rv == -1) {
+           perror("select"); // error occurred in select()
+       } else if (rv == 0) {
+           break;
+       } else {
+           // if there is data to be read
+           if (FD_ISSET(connection, &readfds)) {
+               recv(connection, buffer, 1024, 0);
+               fputs(buffer, fp);
+           }
+       }
+    }
+    fclose(fp);
+    return tempname;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[1024];
+    char *plaintext;
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    int yes = 1;
+
+    if (argc < 2) {
+      fprintf(stderr,"ERROR, no port provided\n");
+      exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+      error("ERROR opening socket");
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    //allows us to reuse the socket
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      perror("setsockopt");
+      exit(1);
+    }
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0)
+        error("ERROR on accept");
+
+
+    plaintext = getFile(newsockfd);
+
+    // FILE *fp;
+    // fp = fopen(plaintext, "r");
+    // char ch;
+    //
+    // while ((ch = getc(fp)) != EOF) {
+    //   if (!isupper(ch) && ch != ' ') {
+    //     printf("found a bad character");
+    //   }
+    // }
+
+
+    bzero(buffer,1024);
+    //:q!printf("Here is the message: %s\n",buffer);
+    n = write(newsockfd,"I got your message",18);
+    if (n < 0) error("ERROR writing to socket");
+    close(newsockfd);
+    close(sockfd);
+    return 0;
 }
